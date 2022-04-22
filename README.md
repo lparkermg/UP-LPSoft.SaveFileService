240a0da032e6744cc9b3d9fc894925e44585c7e1
Luke Parker <moongateuk@gmail.com>
2022-04-22 20:43:48 +0100

    Initial Version - 1.0.0 (#1)
    * Adds SaveService and sets up project.
    
    * Adds missing interface.

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# UP-LPSoft.SaveFileService
-Generic service for saving and loading saves, as a Unity package
+# LPSoft.SaveFileServices
+Generic service for saving and loading saves.
+
+## Usage
+
+###  Service Setup
+
+Initialising the included `SaveService` with a maximum slot value will set an array of the provided data type to the amount of slots provided in the constructor. For example the following will setup a new service with 5 slots of the `string` data type:
+
+```
+var service = new SaveService<string>(5);
+```
+
+### Saving Data
+
+To save the current slots in the service you will need to run `Save("path/to/save/file")` with the file you wish to save to. Running this will allow you to save the data to file on some kind of persistant storage.
+
+### Loading Data
+
+To load previously saved slots in the service you will need to run `Load("path/to/save/file)` with the file you wish to load from. Running this will allow you to load an existing set of saves from a previously saved file.
+
+### Getting a slot
+
+To get a slot from the service you will need to run `Get(index of the slot)` this will return the slot data for usage.
+
+### Setting a slot
+
+TO set a slot in the service you will need to run `Set(index of the slot)` this will overwrite the existing slot data.
+
+## Planned features/changes
+
+These will be implemented at some point in the future:
+
+- Callback to confirm setting data
+- Callback to confirm loading data
+- Enforcing slot size
+    - Currently it is possible to load a file that would increase or decrease the slot size.
+- Wrap exceptions from loading and saving in service specific exceptions.
---
+++ Runtime/ISaveFileService.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="ISaveFileService.cs" company="Luke Parker">
+// Copyright (c) Luke Parker. All rights reserved.
+// </copyright>
+
+namespace LPSoft.SaveFileService
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Interface for the Save File Service.
+    /// </summary>
+    /// <typeparam name="TData">The data type of the saves.</typeparam>
+    public interface ISaveFileService<TData>
+    {
+        /// <summary>
+        /// Sets a specified slot with the new save data, overwriting what was in the internal state.
+        /// </summary>
+        /// <param name="index">Index of the data to update.</param>
+        /// <param name="newData">The new save data.</param>
+        /// <returns>A task representing the async method.</returns>
+        Task Set(int index, TData newData);
+
+        /// <summary>
+        /// Gets a slot of save data from the internal state.
+        /// </summary>
+        /// <param name="index">The index of the slot.</param>
+        /// <returns>The save data in the slot of the provided index.</returns>
+        Task<TData> Get(int index);
+
+        /// <summary>
+        /// Saves all the current slots to file.
+        /// </summary>
+        /// <param name="path">The file path to save to.</param>
+        /// <returns>A task representing the async method.</returns>
+        Task Save(string path);
+
+        /// <summary>
+        /// Loads file slots into the internal state.
+        /// </summary>
+        /// <param name="path">The file path to load from.</param>
+        /// <returns>A task representing the async method.</returns>
+        Task Load(string path);
+    }
+}
